261a08a5a4fa3215ccf539c4c65250688866d32d
Cheburek (Паша) <pasha.hu@yandex.ru>
2024-02-24 12:25:33 +0300

    create solution

---
+++ ClassLibrary/Discipline.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    internal class Discipline
+    {
+        private string Name;
+        private string ShortName;
+
+        public Discipline(string Name1, string ShortName1)
+        {
+            Name = Name1;
+            ShortName = ShortName1;
+        }
+
+        //https://docs.google.com/document/d/1qCEMkbMRvvHP361IFDeWgSpHfVRUp6BV67nrsjl3fSw/edit?usp=drive_link
+        public string name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        public string shname
+        {
+            get { return ShortName; }
+            set { ShortName = value; }
+        }
+    }
+}
---
+++ ClassLibrary/Pair.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Pair
+    {
+        private DateTime startTime;
+        private DateTime endTime;
+        private DateTime breakStartTime;
+        private DateTime breakEndTime;
+        //private OtherClass otherClass;
+
+        public Pair(DateTime startTime, DateTime endTime, DateTime breakStartTime, DateTime breakEndTime, string shiftLink)
+        {
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.breakStartTime = breakStartTime;
+            this.breakEndTime = breakEndTime;
+            //this.otherClass = new OtherClass(shiftLink);
+        }
+    }
+}
