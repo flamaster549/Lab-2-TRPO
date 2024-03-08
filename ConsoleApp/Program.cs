38b45dff9ba457d1c628b89b3716e7decb9b3581
Fedor <gucinskijfedor@gmail.com>
2024-03-08 22:16:43 +0300

    Created_object_in_ConsoleApp

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var TRPO = new ClassLibrary.Discipline("Технология разработки программного обеспечения", "ТРПО");
+            Console.WriteLine($"{TRPO.Name} {TRPO.Shname}");
         }
     }
 }
