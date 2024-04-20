6fc08c7d786f618199387cbbaf1e06752a9699d0
Cheburek (Паша) <pasha.hu@yandex.ru>
2024-04-20 12:10:45 +0300

    Побличные классы

--- ConsoleApp/Fabric.cs
+++ ConsoleApp/Fabric.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary;
 
 namespace ConsoleApp
 {
-    internal class Program
+    internal static class Fabric
     {
-        static void Main()
+        public static Employee CreateEmploeyy()
         {
+            return null;
         }
+
+        
     }
 }
