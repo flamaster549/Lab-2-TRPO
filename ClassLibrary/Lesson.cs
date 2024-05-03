3243dfd91b677e93454cd565b47b23a1c2a6ffbc
flamaster549 <cheesnik13@gmail.com>
2024-05-03 12:33:34 +0300

    Update Lesson.cs
    дейт тайм и свойства

--- ClassLibrary/Lesson.cs
+++ ClassLibrary/Lesson.cs
@@ -8,22 +8,60 @@ namespace ClassLibrary
 {
     internal class Lesson
     {
-        private DateTime datetime;
+        private DateTime date;
         private Discipline discipline;
         private Employee employee;
         private Classroom classroom;
         private Group group;
         private Pair pair;
 
-        public Lesson(DateTime datetime, Discipline discipline, Employee employee, Classroom classroom, Group group, Pair pair)
+        public Lesson(Discipline discipline, Employee employee, Classroom classroom, Group group, Pair pair, DateTime? date = null)
         {
-            this.datetime = datetime;
+            this.date = date?? DateTime.Now;
             this.discipline = discipline;
             this.employee = employee;
             this.classroom = classroom;
             this.group = group;
             this.pair = pair;
         }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public Discipline Discipline
+        {
+            get { return discipline; }
+            set { discipline = value; }
+        }
+
+        public Employee Employee
+        {
+            get { return employee; }
+            set { employee = value; }
+        }
+
+        public Classroom Classroom
+        {
+            get { return classroom; }
+            set { classroom = value; }
+        }
+
+        public Group Group
+        {
+            get { return group; }
+            set { group = value; }
+        }
+
+        public Pair Pair
+        {
+            get { return pair; }
+            set { pair = value; }
+        }
+
+
     }
- 
+
 }
