47b6fccc97847cbe27fe9f283d9689421b3ecbc5
search1212 <150236324+search1212@users.noreply.github.com>
2024-05-04 13:20:14 +0300

    Update Pair.cs

--- ClassLibrary/Pair.cs
+++ ClassLibrary/Pair.cs
@@ -6,21 +6,45 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    public class Pair
+   internal class Pair
     {
         private DateTime startTime;
         private DateTime endTime;
         private DateTime breakStartTime;
         private DateTime breakEndTime;
-        //private OtherClass otherClass;
-
-        public Pair(DateTime startTime, DateTime endTime, DateTime breakStartTime, DateTime breakEndTime, string shiftLink)
+        private WorkShift workShift;
+        public Pair(DateTime startTime1, DateTime endTime1, DateTime breakStartTime1, DateTime breakEndTime1, WorkShift workShift1)
         {
-            this.startTime = startTime;
-            this.endTime = endTime;
-            this.breakStartTime = breakStartTime;
-            this.breakEndTime = breakEndTime;
-            //this.otherClass = new OtherClass(shiftLink);
+            startTime = startTime1 != DateTime.MinValue ? startTime1 : DateTime.Now;
+            endTime = endTime1 != DateTime.MinValue ? endTime1 : DateTime.Now;
+            breakStartTime = breakStartTime != DateTime.MinValue ? breakStartTime : DateTime.Now;
+            breakEndTime = breakEndTime1 != DateTime.MinValue ? breakEndTime1 : DateTime.Now;
+            workShift = workShift1;
+        }
+        public WorkShift WorkShift
+        {
+            get { return workShift; }
+            set { workShift = value; }
+        }
+        public DateTime StartTime
+        {
+            get { return startTime; }
+            set { startTime = value; }
+        }
+        public DateTime EndTime
+        {
+            get { return endTime; }
+            set { endTime = value; }
+        }
+        public DateTime BreakStartTime
+        {
+            get { return breakStartTime; }
+            set { breakStartTime = value; }
+        }
+        public DateTime BreakEndTime
+        {
+            get { return breakEndTime; }
+            set { breakEndTime = value; }
         }
     }
 }
